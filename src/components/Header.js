481852c41c860eb4d17cff6cc615c7592b044739
Scotty Yackzan <syackzan@gmail.com>
2023-08-26 09:09:36 +0200

    Added style changes to Header and body of portfolio to create a more grid like structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,39 @@
 import React from 'react';
 
-function Header({ currentPage, handlePageChange}) {
+function Header({ currentPage, handlePageChange }) {
     return (
-        <header className="headerStyle">
-            <div className="full centerElement onHover">
-                <p className="headerTextStyle">SCOTT YACKZAN PORTFOLIO</p>
-            </div>
-            <div className="d-flex full">
-                <div className="divLinksA full">
-                    <a className="divLinksB full" href="#about" onClick={() => handlePageChange('About')}>
-                        <p className={currentPage === 'About' ? 'navStyle' : 'navStyle'}>ABOUT</p>
-                    </a>
-                    <a className="divLinksB full" href="#portfolio" onClick={() => handlePageChange('Portfolio')}>
-                        <p className={currentPage === 'Portfolio' ? 'navStyle' : 'navStyle'}>PORTFOLIO</p>
-                    </a>
-                    <a className="divLinksB full" href="#resume" onClick={() => handlePageChange('Resume')}>
-                        <p className={currentPage === 'Resume' ? 'navStyle' : 'navStyle'}>RESUME</p>
-                    </a>
-                    <a className="divLinksB full" href="#contact" onClick={() => handlePageChange('Contact')}>
-                        <p className={currentPage === 'Contact' ? 'navStyle' : 'navStyle'}>CONTACT</p>
-                    </a>
+        <div>
+            <header className="headerStyle">
+                <div className="full centerElement onHover">
+                    <p className="headerTextStyle">SCOTT YACKZAN</p>
+                </div>
+                <div className="d-flex full">
+                    <div className="divLinksA full">
+                        <a className="divLinksB full" href="#portfolio" onClick={() => handlePageChange('Portfolio')}>
+                            <p className={currentPage === 'Portfolio' ? 'navStyle' : 'navStyle'}>PORTFOLIO</p>
+                        </a>
+                        <a className="divLinksB full" href="#resume" onClick={() => handlePageChange('Resume')}>
+                            <p className={currentPage === 'Resume' ? 'navStyle' : 'navStyle'}>RESUME</p>
+                        </a>
+                        <a className="divLinksB full" href="#about" onClick={() => handlePageChange('About')}>
+                            <p className={currentPage === 'About' ? 'navStyle' : 'navStyle'}>ABOUT ME</p>
+                        </a>
+                        <a className="divLinksB full" href="#contact" onClick={() => handlePageChange('Contact')}>
+                            <p className={currentPage === 'Contact' ? 'navStyle' : 'navStyle'}>CONTACT</p>
+                        </a>
+                    </div>
+                </div>
+            </header>
+            <div className="bottomHeader">
+                <div className="col-6 full centerElement">
+                    <p className="headerTextStyle">PORTFOLIO</p>
+                </div>
+                <div className="col-6">
+
                 </div>
             </div>
-        </header>
+        </div>
+
     )
 }
 
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -4,39 +4,68 @@ import techBlog from '../assets/images/techblog.png';
 
 function Portfolio() {
     return (
-    <div>
-        <div className="jumbotron aboutMe">
-            <section class="header1">
+        <div>
+            <div className="portfolioContainer">
+                {/* <section class="header1">
                 <h2 className="headerColor"><u>Full Stack Programs</u></h2>
-            </section>
-            <div className="row d-flex flex-wrap">
-                <div className="col-md-6 columnDiv">
-                    <div className="order">
-                        <a href="https://sheltered-lake-23144.herokuapp.com/"
-                            className="navStyle">
-                            <section className="mainBorder formatSection bigger">
-                                <img className="formatImage" src={onTrack} alt="onTrack Image" />
-                                <h2 className="formatHeader">onTrack</h2>
-                            </section>
-                        </a>
+            </section> */}
+                <a href="https://sheltered-lake-23144.herokuapp.com/" className="square">
+                    <div class="content">
+                        <h1 class="thumb-title">ON TRACK</h1>
+                        <div class="image-container">
+                            <img src={onTrack} alt="Image" />
+                        </div>
+                        <h2 class="thumb-subtitle">NOSQL database</h2>
                     </div>
-                </div>
-                <div className="col-md-6 columnDiv">
-                    <div className="order">
+                </a>
+                <div className="square">
+                    {/* <div className="">
                         <a href="https://cryptic-sands-14911.herokuapp.com/"
                             className="navStyle">
-                            <section className="mainBorder formatSection bigger">
+                            <section className="mainBorder formatSection">
                                 <img className="formatImage" src={techBlog} alt="techBlog Image" />
                                 <h2 className="formatHeader">Tech Blog</h2>
                             </section>
                         </a>
-                    </div>
+                    </div> */}
+                </div>
+                <div className="square">
+                    {/* <div className="">
+                        <a href="https://cryptic-sands-14911.herokuapp.com/"
+                            className="navStyle">
+                            <section className="mainBorder formatSection">
+                                <img className="formatImage" src={techBlog} alt="techBlog Image" />
+                                <h2 className="formatHeader">Tech Blog</h2>
+                            </section>
+                        </a>
+                    </div> */}
+                </div>
+                <div className="square">
+                    {/* <div className="">
+                        <a href="https://cryptic-sands-14911.herokuapp.com/"
+                            className="navStyle">
+                            <section className="mainBorder formatSection">
+                                <img className="formatImage" src={techBlog} alt="techBlog Image" />
+                                <h2 className="formatHeader">Tech Blog</h2>
+                            </section>
+                        </a>
+                    </div> */}
+                </div>
+                <div className="square">
+                    {/* <div className="">
+                        <a href="https://cryptic-sands-14911.herokuapp.com/"
+                            className="navStyle">
+                            <section className="mainBorder formatSection">
+                                <img className="formatImage" src={techBlog} alt="techBlog Image" />
+                                <h2 className="formatHeader">Tech Blog</h2>
+                            </section>
+                        </a>
+                    </div> */}
                 </div>
             </div>
         </div>
-    </div>
 
-    
+
     )
 }
 
